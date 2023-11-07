462dfe7ee1a634d0556dc481d896d42420265ce8
Tiago Domingos <tiagohdomingos@hotmail.com>
2023-11-07 11:43:55 -0300

    Defit alterações

--- .vscode/script.js
+++ .vscode/script.js
@@ -2,11 +2,23 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  const img = document.querySelector("profile img")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
+  // » modo explicado do toggle «
+  // if(html.classList.contains('light')) {
+  // html.classList.remove('light')
+  // } else {
+  //  html.classlist.add('light')}
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/defit cdr logo 2 para pagina light.png")
+    img.setAttribute("alt", "Foto logo Defit light")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/defit cdr logo 2 para pagina.png")
+    img.setAttribute("alt", "Foto logo Defit")
   }
 }
